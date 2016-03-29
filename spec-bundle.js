0ef0e72a6f30804603407a5b53a8c086dc7b096d
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-29 16:09:06 +0000

    fix(test): provide Karma with appropriate Zone.js module

--- spec-bundle.js
+++ spec-bundle.js
@@ -17,7 +17,7 @@ Error.stackTraceLimit = Infinity;
 // Prefer `CoreJS` over the polyfills commented out above
 require('core-js');
 
-require('zone.js/dist/zone-microtask.js');
+require('zone.js/dist/zone.js');
 require('zone.js/dist/long-stack-trace-zone.js');
 require('zone.js/dist/jasmine-patch.js');
 
