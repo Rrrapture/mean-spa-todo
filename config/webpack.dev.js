cf6880934d203eee4c22a5cc77ab847da5afa725
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-29 23:40:55 +0000

    feat(webpack): leverage webpack-merge

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,92 +6,104 @@
 
 //# Common Configuration and Helpers
 var helpers = require('./helpers');
-var config = require('./webpack.common.js');
+// Use `webpack-merge` to merge configs
+var webpackMerge = require('webpack-merge');
+// Common `webpack` configuration for `dev` and `prod`
+var commonConfig = require('./webpack.common.js');
 
 //# Environment Config Object
 var envConfig = require('./config.json');
 
-// Developer tool to enhance debugging
-//
-// See: http://webpack.github.io/docs/configuration.html#devtool
-// See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
-config.devtool: 'cheap-module-eval-source-map';
-// Switch loaders to debug mode.
-//
-// See: http://webpack.github.io/docs/configuration.html#debug
-config.debug: true;
 
-// Options affecting the output of the compilation.
-//
-// See: http://webpack.github.io/docs/configuration.html#output
-config.output: {
-  // The output directory as absolute path (required).
+module.exports = webpackMerge(commonConfig, {
+
+  // Developer tool to enhance debugging
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-path
-  path: helpers.root('dist'),
-  // Specifies the name of each output file on disk.
-  // IMPORTANT: You must not specify an absolute path here!
+  // See: http://webpack.github.io/docs/configuration.html#devtool
+  // See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+  devtool: 'cheap-module-eval-source-map',
+  // Switch loaders to debug mode.
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-filename
-  filename: '[name].bundle.js',
-  // The filename of the SourceMaps for the JavaScript files.
-  // They are inside the output.path directory.
+  // See: http://webpack.github.io/docs/configuration.html#debug
+  debug: true,
+
+  // Options affecting the output of the compilation.
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
-  sourceMapFilename: '[name].map',
-  // The filename of non-entry chunks as relative path
-  // inside the output.path directory.
-  //
-  // See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
-  chunkFilename: '[id].chunk.js'
-};
-
-// Add additional plugins to the compiler.
-//
-// See: http://webpack.github.io/docs/configuration.html#plugins
-config.plugins.push([
-
-  // TODO(datatypevoid): investigate the necessity of these two
-  // following lines
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NoErrorsPlugin()
-]);
-
-// Other module loader config
-
-// Static analysis linter for TypeScript advanced options configuration
-// Description: An extensible linter for the TypeScript language.
-//
-// See: https://github.com/wbuchwalter/tslint-loader
-config.tslint: {
-  emitErrors: false,
-  failOnHint: false,
-  resourcePath: 'src',
-};
-
-// Webpack Development Server configuration
-// Description: The webpack-dev-server is a little node.js Express server.
-// The server emits information about the compilation state to the client,
-// which reacts to those events.
-//
-// See: https://webpack.github.io/docs/webpack-dev-server.html
-config.devServer: {
-  // Proxy requests to our `Express` server
-  proxy: {
-    '*': {
-      target: 'http://localhost:' + envConfig.PORT,
-      secure: false
-    },
+  // See: http://webpack.github.io/docs/configuration.html#output
+  output: {
+    // The output directory as absolute path (required).
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-path
+    path: helpers.root('dist'),
+    // Specifies the name of each output file on disk.
+    // IMPORTANT: You must not specify an absolute path here!
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-filename
+    filename: '[name].bundle.js',
+    // The filename of the SourceMaps for the JavaScript files.
+    // They are inside the output.path directory.
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+    sourceMapFilename: '[name].map',
+    // The filename of non-entry chunks as relative path
+    // inside the output.path directory.
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+    chunkFilename: '[id].chunk.js'
   },
-  port: METADATA.port,
-  host: METADATA.host,
-  historyApiFallback: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
+
+  // Add additional plugins to the compiler.
+  //
+  // See: http://webpack.github.io/docs/configuration.html#plugins
+  plugins: [
+
+    // TODO(datatypevoid): investigate the necessity of these two
+    // following lines
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+
+  // Other module loader config
+
+  // Static analysis linter for TypeScript advanced options configuration
+  // Description: An extensible linter for the TypeScript language.
+  //
+  // See: https://github.com/wbuchwalter/tslint-loader
+  tslint: {
+    emitErrors: false,
+    failOnHint: false,
+    resourcePath: 'src',
+  },
+
+  // Webpack Development Server configuration
+  // Description: The webpack-dev-server is a little node.js Express server.
+  // The server emits information about the compilation state to the client,
+  // which reacts to those events.
+  //
+  // See: https://webpack.github.io/docs/webpack-dev-server.html
+  devServer: {
+    // Proxy requests to our `Express` server
+    proxy: {
+      '*': {
+        target: 'http://localhost:' + envConfig.PORT,
+        secure: false
+      },
+    },
+    port: METADATA.port,
+    host: METADATA.host,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    }
+  },
+
+  node: {
+    global: 'window',
+    crypto: 'empty',
+    process: true,
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
   }
-};
-
-config.node.process = true;
-
-module.exports = config;
+});
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -4,8 +4,12 @@
 // webpack.prod.js may be freely distributed under the MIT license
 // ```
 
-var helpers = require('./helpers'); // Helper: root(), and rootDir() are defined at the bottom
-var config = require('./webpack.common.js');
+//# Common Configuration and Helpers
+var helpers = require('./helpers');
+// Use `webpack-merge` to merge configs
+var webpackMerge = require('webpack-merge');
+// Common `webpack` configuration for `dev` and `prod`
+var commonConfig = require('./webpack.common.js');
 
 // Webpack Plugins
 var ProvidePlugin = require('webpack/lib/ProvidePlugin');
@@ -14,178 +18,186 @@ var UglifyJsPlugin = require('webpack/lib/optimize/UglifyJsPlugin');
 var CompressionPlugin = require('compression-webpack-plugin');
 var WebpackMd5Hash = require('webpack-md5-hash');
 
-// Developer tool to enhance debugging
-//
-// See: http://webpack.github.io/docs/configuration.html#devtool
-// See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
-config.devtool: 'source-map';
-// Switch loaders to debug mode.
-//
-// See: http://webpack.github.io/docs/configuration.html#debug
-config.debug: false;
-
-// Options affecting the output of the compilation.
-//
-// See: http://webpack.github.io/docs/configuration.html#output
-config.output: {
-  // The output directory as absolute path (required).
+module.exports = webpackMerge(commonConfig, {
+  // Developer tool to enhance debugging
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-path
-  path: helpers.root('dist'),
-  // Specifies the name of each output file on disk.
-  // IMPORTANT: You must not specify an absolute path here!
+  // See: http://webpack.github.io/docs/configuration.html#devtool
+  // See: https://github.com/webpack/docs/wiki/build-performance#sourcemaps
+  devtool: 'source-map',
+  // Switch loaders to debug mode.
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-filename
-  filename: '[name].[chunkhash].bundle.js',
-  // The filename of the SourceMaps for the JavaScript files.
-  // They are inside the output.path directory.
+  // See: http://webpack.github.io/docs/configuration.html#debug
+  debug: false,
+
+  // Options affecting the output of the compilation.
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
-  sourceMapFilename: '[name].[chunkhash].bundle.map',
-  // The filename of non-entry chunks as relative path
-  // inside the output.path directory.
+  // See: http://webpack.github.io/docs/configuration.html#output
+  output: {
+    // The output directory as absolute path (required).
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-path
+    path: helpers.root('dist'),
+    // Specifies the name of each output file on disk.
+    // IMPORTANT: You must not specify an absolute path here!
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-filename
+    filename: '[name].[chunkhash].bundle.js',
+    // The filename of the SourceMaps for the JavaScript files.
+    // They are inside the output.path directory.
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+    sourceMapFilename: '[name].[chunkhash].bundle.map',
+    // The filename of non-entry chunks as relative path
+    // inside the output.path directory.
+    //
+    // See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+    chunkFilename: '[id].[chunkhash].chunk.js'
+  },
+
+  // Add additional plugins to the compiler.
   //
-  // See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
-  chunkFilename: '[id].[chunkhash].chunk.js'
-};
+  // See: http://webpack.github.io/docs/configuration.html#plugins
+  plugins: [
+    // Plugin: WebpackMd5Hash
+    // Description: Plugin to replace a standard webpack chunkhash with md5.
+    //
+    // See: https://www.npmjs.com/package/webpack-md5-hash
+    new WebpackMd5Hash(),
 
-config.plugins.push([
-  // Plugin: WebpackMd5Hash
-  // Description: Plugin to replace a standard webpack chunkhash with md5.
-  //
-  // See: https://www.npmjs.com/package/webpack-md5-hash
-  new WebpackMd5Hash(),
+    // Plugin: DedupePlugin
+    // Description: Prevents the inclusion of duplicate code into your bundle
+    // and instead applies a copy of the function at runtime.
+    //
+    // See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
+    // See: https://github.com/webpack/docs/wiki/optimization#deduplication
+    new DedupePlugin(),
 
-  // Plugin: DedupePlugin
-  // Description: Prevents the inclusion of duplicate code into your bundle
-  // and instead applies a copy of the function at runtime.
-  //
-  // See: https://webpack.github.io/docs/list-of-plugins.html#defineplugin
-  // See: https://github.com/webpack/docs/wiki/optimization#deduplication
-  new DedupePlugin(),
+    // Plugin: UglifyJsPlugin
+    // Description: Minimize all JavaScript output of chunks.
+    // Loaders are switched into minimizing mode.
+    //
+    // See: https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
+    // NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
+    new UglifyJsPlugin({
+      // beautify: true, //debug
+      // mangle: false, //debug
+      // dead_code: false, //debug
+      // unused: false, //debug
+      // deadCode: false, //debug
+      // compress: {
+      //   screw_ie8: true,
+      //   keep_fnames: true,
+      //   drop_debugger: false,
+      //   dead_code: false,
+      //   unused: false
+      // }, // debug
+      // comments: true, //debug
 
-  // Plugin: UglifyJsPlugin
-  // Description: Minimize all JavaScript output of chunks.
-  // Loaders are switched into minimizing mode.
-  //
-  // See: https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-  // NOTE: To debug prod builds uncomment //debug lines and comment //prod lines
-  new UglifyJsPlugin({
-    // beautify: true, //debug
-    // mangle: false, //debug
-    // dead_code: false, //debug
-    // unused: false, //debug
-    // deadCode: false, //debug
-    // compress: {
-    //   screw_ie8: true,
-    //   keep_fnames: true,
-    //   drop_debugger: false,
-    //   dead_code: false,
-    //   unused: false
-    // }, // debug
-    // comments: true, //debug
+      beautify: false, //prod
 
-    beautify: false, //prod
+      // mangle: { screw_ie8 : true }, //prod
+      mangle: {
+        screw_ie8: true,
+        except: [
+            'App',
+            'About',
+            'Contact',
+            'Home',
+            'Menu',
+            'Footer',
+            'XLarge',
+            'RouterActive',
+            'RouterLink',
+            'RouterOutlet',
+            'NgFor',
+            'NgIf',
+            'NgClass',
+            'NgSwitch',
+            'NgStyle',
+            'NgSwitchDefault',
+            'NgControl',
+            'NgControlName',
+            'NgControlGroup',
+            'NgFormControl',
+            'NgModel',
+            'NgFormModel',
+            'NgForm',
+            'NgSelectOption',
+            'DefaultValueAccessor',
+            'NumberValueAccessor',
+            'CheckboxControlValueAccessor',
+            'SelectControlValueAccessor',
+            'RadioControlValueAccessor',
+            'NgControlStatus',
+            'RequiredValidator',
+            'MinLengthValidator',
+            'MaxLengthValidator',
+            'PatternValidator',
+            'AsyncPipe',
+            'DatePipe',
+            'JsonPipe',
+            'NumberPipe',
+            'DecimalPipe',
+            'PercentPipe',
+            'CurrencyPipe',
+            'LowerCasePipe',
+            'UpperCasePipe',
+            'SlicePipe',
+            'ReplacePipe',
+            'I18nPluralPipe',
+            'I18nSelectPipe'
+          ] // Needed for uglify RouterLink problem
+      }, // prod
+      compress: {
+        screw_ie8: true
+      }, //prod
+      comments: false //prod
+    }),
 
-    // mangle: { screw_ie8 : true }, //prod
-    mangle: {
-      screw_ie8: true,
-      except: [
-          'App',
-          'About',
-          'Contact',
-          'Home',
-          'Menu',
-          'Footer',
-          'XLarge',
-          'RouterActive',
-          'RouterLink',
-          'RouterOutlet',
-          'NgFor',
-          'NgIf',
-          'NgClass',
-          'NgSwitch',
-          'NgStyle',
-          'NgSwitchDefault',
-          'NgControl',
-          'NgControlName',
-          'NgControlGroup',
-          'NgFormControl',
-          'NgModel',
-          'NgFormModel',
-          'NgForm',
-          'NgSelectOption',
-          'DefaultValueAccessor',
-          'NumberValueAccessor',
-          'CheckboxControlValueAccessor',
-          'SelectControlValueAccessor',
-          'RadioControlValueAccessor',
-          'NgControlStatus',
-          'RequiredValidator',
-          'MinLengthValidator',
-          'MaxLengthValidator',
-          'PatternValidator',
-          'AsyncPipe',
-          'DatePipe',
-          'JsonPipe',
-          'NumberPipe',
-          'DecimalPipe',
-          'PercentPipe',
-          'CurrencyPipe',
-          'LowerCasePipe',
-          'UpperCasePipe',
-          'SlicePipe',
-          'ReplacePipe',
-          'I18nPluralPipe',
-          'I18nSelectPipe'
-        ] // Needed for uglify RouterLink problem
-    }, // prod
-    compress: {
-      screw_ie8: true
-    }, //prod
-    comments: false //prod
-  }),
-
-  // Plugin: CompressionPlugin
-  // Description: Prepares compressed versions of assets to serve
-  // them with Content-Encoding
-  //
-  // See: https://github.com/webpack/compression-webpack-plugin
-  new CompressionPlugin({
-    algorithm: helpers.gzipMaxLevel,
-    regExp: /\.css$|\.html$|\.js$|\.map$/,
-    threshold: 2 * 1024
-  })
-]);
-
-
-
-// Static analysis linter for TypeScript advanced options configuration
-// Description: An extensible linter for the TypeScript language.
-//
-// See: https://github.com/wbuchwalter/tslint-loader
-config.tslint = {
-  emitErrors: true,
-  failOnHint: true,
-  resourcePath: 'src'
-};
-
-// Html loader advanced options
-//
-// See: https://github.com/webpack/html-loader#advanced-options
-// TODO: Need to workaround Angular 2's html syntax => #id [bind] (event) *ngFor
-config.htmlLoader = {
-  minimize: true,
-  removeAttributeQuotes: false,
-  caseSensitive: true,
-  customAttrSurround: [
-    [/#/, /(?:)/],
-    [/\*/, /(?:)/],
-    [/\[?\(?/, /(?:)/]
+    // Plugin: CompressionPlugin
+    // Description: Prepares compressed versions of assets to serve
+    // them with Content-Encoding
+    //
+    // See: https://github.com/webpack/compression-webpack-plugin
+    new CompressionPlugin({
+      algorithm: helpers.gzipMaxLevel,
+      regExp: /\.css$|\.html$|\.js$|\.map$/,
+      threshold: 2 * 1024
+    })
   ],
-  customAttrAssign: [/\)?\]?=/]
-};
 
-config.node.process = false;
+  // Static analysis linter for TypeScript advanced options configuration
+  // Description: An extensible linter for the TypeScript language.
+  //
+  // See: https://github.com/wbuchwalter/tslint-loader
+  tslint: {
+    emitErrors: true,
+    failOnHint: true,
+    resourcePath: 'src'
+  },
 
-module.exports = config;
+  // Html loader advanced options
+  //
+  // See: https://github.com/webpack/html-loader#advanced-options
+  // TODO: Need to workaround Angular 2's html syntax => #id [bind] (event) *ngFor
+  htmlLoader: {
+    minimize: true,
+    removeAttributeQuotes: false,
+    caseSensitive: true,
+    customAttrSurround: [
+      [/#/, /(?:)/],
+      [/\*/, /(?:)/],
+      [/\[?\(?/, /(?:)/]
+    ],
+    customAttrAssign: [/\)?\]?=/]
+  },
+
+  node: {
+    global: 'window',
+    crypto: 'empty',
+    process: false,
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+});
