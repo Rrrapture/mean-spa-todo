af8738d2c44fd1b4f87b284228cc418ad8371fbb
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-29 20:54:13 +0000

    chore: comment out material 2

--- src/platform/browser/directives.ts
+++ src/platform/browser/directives.ts
@@ -4,27 +4,28 @@ import {provide, PLATFORM_DIRECTIVES} from 'angular2/core';
 import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 // Angular 2 Material
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {MdToolbar} from '@angular2-material/toolbar';
-import {MdButton} from '@angular2-material/button';
-import {MdCheckbox} from '@angular2-material/checkbox';
-import {MdRadioButton} from '@angular2-material/radio';
-import {MdSpinner} from '@angular2-material/progress-circle';
-import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+// TODO(datatypevoid): uncomment when material is fixed
+// import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+// import {MdToolbar} from '@angular2-material/toolbar';
+// import {MdButton} from '@angular2-material/button';
+// import {MdCheckbox} from '@angular2-material/checkbox';
+// import {MdRadioButton} from '@angular2-material/radio';
+// import {MdSpinner} from '@angular2-material/progress-circle';
+// import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
+// const MATERIAL_DIRECTIVES = [
+//   ...MD_SIDENAV_DIRECTIVES,
+//   ...[ MdToolbar, MdButton, MdCheckbox, MdRadioButton, MdSpinner ],
+//   ...MD_CARD_DIRECTIVES
+// ];
 
-// Common Platform directives
-import {RouterActive} from './router-active';
-
-// application_directives: directives that are global through out the application
+// APPLICATION_DIRECTIVES
+//
+// directives that are global through out the application
 export const APPLICATION_DIRECTIVES = [
 
   ...ROUTER_DIRECTIVES,
-
-  ...MD_SIDENAV_DIRECTIVES,
-  ...[ MdToolbar, MdButton, MdCheckbox, MdRadioButton, MdSpinner ],
-  ...MD_CARD_DIRECTIVES,
-
-  RouterActive
+  // TODO(datatypevoid): uncomment when material is fixed
+  //...MATERIAL_DIRECTIVES
 ];
 
 export const DIRECTIVES = [
