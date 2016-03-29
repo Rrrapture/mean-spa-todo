94aba7f241c09e3a57c60356f5ca9451a563ea20
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-29 19:48:57 +0000

    feat(platform): pipes

---
+++ src/platform/browser/pipes.ts
@@ -0,0 +1,12 @@
+import {provide, PLATFORM_PIPES} from 'angular2/core';
+
+//# APPLICATION_PIPES
+//
+//** Pipes that are global throughout our application **
+export const APPLICATION_PIPES = [
+
+];
+
+export const PIPES = [
+  provide(PLATFORM_PIPES, { useValue: APPLICATION_PIPES, multi: true })
+];
