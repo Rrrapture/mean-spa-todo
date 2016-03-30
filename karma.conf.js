005b41a9d8c32ca89d42a93d36601439f3ce16cd
datatypevoid <david.r.niciforovic@gmail.com>
2016-03-30 15:40:17 +0000

    fix(karma.conf): fix ref to webpack.test

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@
 
 module.exports = function(config) {
 
-  var testWebpackConfig = require('./webpack.test.config.js');
+  var testWebpackConfig = require('./config/webpack.test.config.js');
   config.set({
 
     // base path that will be used to resolve all patterns (e.g. files, exclude)
