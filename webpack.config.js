15965c787b1df9a292852e96c43381faa51602f5
datatypevoid <david.r.niciforovic@gmail.com>
2016-05-16 14:05:31 -0400

    feat(webpack.config): switch environments

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,18 @@
 // webpack.config.js may be freely distributed under the MIT license
 // ```
 
-// Look in `./config` folder for `webpack.dev.js`
-module.exports = require('./config/webpack.dev.js');
+// Look in `./config` folder for `webpack.*.config.js`
+switch (process.env.NODE_ENV) {
+  case 'prod':
+  case 'production':
+    module.exports = require('./config/webpack.prod');
+    break;
+  case 'test':
+  case 'testing':
+    module.exports = require('./config/webpack.test');
+    break;
+  case 'dev':
+  case 'development':
+  default:
+    module.exports = require('./config/webpack.dev');
+}
