254b8daaf86708a804298a04f38d4005610b2697
datatypevoid <david.r.niciforovic@gmail.com>
2016-05-16 13:34:46 -0400

    chore(polyfills.ts): prefer specific deps import

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -7,12 +7,12 @@
 // import 'es7-reflect-metadata';
 
 // Prefer CoreJS over the polyfills above
-import 'core-js';
+import 'core-js/es6';
+import 'core-js/es7/reflect';
 require('zone.js/dist/zone');
 
 if ('production' === ENV) {
   // Production
-
 
 } else {
   // Development
