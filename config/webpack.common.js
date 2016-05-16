dc9e68ee7df6b66640555305bd57427be0d09a42
datatypevoid <david.r.niciforovic@gmail.com>
2016-05-16 17:43:15 -0400

    fix(webpack.common): correct function name

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -196,7 +196,7 @@ module.exports = {
     // See: https://webpack.github.io/docs/list-of-plugins.html#commonschunkplugin
     // See: https://github.com/webpack/docs/wiki/optimization#multi-page-app
     new webpack.optimize.CommonsChunkPlugin({
-      name: ['polyfills', 'vendor'].revese()
+      name: ['polyfills', 'vendor'].reverse()
     }),
 
     // Plugin: CopyWebpackPlugin
