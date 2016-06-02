e8baac49cce149ce1a42982b81ce8015fadd9279
datatypevoid <david.r.niciforovic@gmail.com>
2016-06-02 19:20:57 +0000

    update material2 to 2.0.0-alpha.5

--- src/platform/browser/angular2-material2/index.ts
+++ src/platform/browser/angular2-material2/index.ts
@@ -1,18 +1,19 @@
-import {MdButton, MdAnchor} from '@angular2-material/button';
-import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
-import {MdCheckbox} from '@angular2-material/checkbox';
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
-import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
-import {MdRadioGroup, MdRadioButton, MdRadioDispatcher} from '@angular2-material/radio';
-import {MdProgressBar} from '@angular2-material/progress-bar';
-import {MdSpinner, MdProgressCircle} from '@angular2-material/progress-circle';
-import {MdToolbar} from '@angular2-material/toolbar';
+import { MdAnchor, MdButton } from '@angular2-material/button';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdCheckbox } from '@angular2-material/checkbox';
+import { MD_GRID_LIST_DIRECTIVES } from '@angular2-material/grid-list';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
-
+import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
+import { MD_LIST_DIRECTIVES } from '@angular2-material/list';
+import { MdProgressBar } from '@angular2-material/progress-bar';
+import { MdProgressCircle, MdSpinner } from '@angular2-material/progress-circle';
+import { MdRadioButton, MdRadioDispatcher, MdRadioGroup } from '@angular2-material/radio';
+import { MD_SIDENAV_DIRECTIVES } from '@angular2-material/sidenav';
+import { MD_SLIDE_TOGGLE_DIRECTIVES } from '@angular2-material/slide-toggle';
+import { MD_TABS_DIRECTIVES } from '@angular2-material/tabs';
+import { MdToolbar } from '@angular2-material/toolbar';
 /*
- * We are grouping the module so we only need to
- * manage the imports in one location
+ * we are grouping the module so we only need to manage the imports in one location
  */
 
 export const MATERIAL_PIPES = [
@@ -20,25 +21,28 @@ export const MATERIAL_PIPES = [
 ];
 
 export const MATERIAL_DIRECTIVES = [
-  ...MD_SIDENAV_DIRECTIVES,
   ...[
     MdAnchor,
     MdButton,
-    MdToolbar,
     MdCheckbox,
+    MdIcon,
+    MdProgressBar,
+    MdProgressCircle,
     MdRadioButton,
     MdRadioGroup,
     MdSpinner,
-    MdProgressBar,
-    MdProgressCircle,
-    MdIcon
+    MdToolbar
   ],
+  ...MD_CARD_DIRECTIVES,
+  ...MD_GRID_LIST_DIRECTIVES,
   ...MD_INPUT_DIRECTIVES,
   ...MD_LIST_DIRECTIVES,
-  ...MD_CARD_DIRECTIVES
+  ...MD_SIDENAV_DIRECTIVES,
+  ...MD_SLIDE_TOGGLE_DIRECTIVES,
+  ...MD_TABS_DIRECTIVES
 ];
 
 export const MATERIAL_PROVIDERS = [
-  MdRadioDispatcher,
-  MdIconRegistry
+  MdIconRegistry,
+  MdRadioDispatcher
 ];
--- src/vendor.ts
+++ src/vendor.ts
@@ -19,14 +19,17 @@ import 'rxjs/add/operator/mergeMap';
 import '@angular2-material/button';
 import '@angular2-material/card';
 import '@angular2-material/checkbox';
-import '@angular2-material/sidenav';
+import '@angular2-material/grid-list';
 import '@angular2-material/input';
 import '@angular2-material/list';
 import '@angular2-material/radio';
 import '@angular2-material/progress-bar';
 import '@angular2-material/progress-circle';
+import '@angular2-material/sidenav';
+import '@angular2-material/slide-toggle';
+import '@angular2-material/tabs';
 import '@angular2-material/toolbar';
-// look in platform/directives and platform/providers
+// look in src/platform/angular2-material2 and src/platform/providers
 
 if ('production' === ENV) {
   // Production
